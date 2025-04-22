90fafb54e3dc4351e15364a5ffba87ce5b52eda8
BOYONG CHEN <brianchen.me@outlook.com>
2025-04-22 20:05:18 +0100

    dependency: add prettier for tailwind for autoformat

--- client/src/components/Plan.jsx
+++ client/src/components/Plan.jsx
@@ -4,13 +4,13 @@ import { X } from "lucide-react";
 export default function Plan({ plan, setSelectedPlan }) {
   return (
     <div
-      className="flex flex-col justify-around w-full px-4 py-2 text-white hover:bg-neutral-600 rounded-2xl transition duration-175"
+      className="flex w-full flex-col justify-around rounded-2xl px-4 py-2 text-white transition duration-175 hover:bg-neutral-600"
       onClick={() => {
         setSelectedPlan(plan);
       }}
     >
-      <p className="text-lg font-medium ">{plan.name}</p>
-      <p className="text-sm font-mono text-gray-100">
+      <p className="text-lg font-medium">{plan.name}</p>
+      <p className="font-mono text-sm text-gray-100">
         {plan.start_date} to {plan.end_date}
       </p>
     </div>
--- client/src/components/PlanForm.jsx
+++ client/src/components/PlanForm.jsx
@@ -55,7 +55,7 @@ export default function PlanForm({
       console.log(formatFormData());
       const res = await axios.post(
         "http://localhost:3000/journey-plans",
-        formatFormData()
+        formatFormData(),
       );
       console.log("Succefully create a journey plan", res.data);
       getAllPlans();
@@ -79,7 +79,7 @@ export default function PlanForm({
 
       const res = await axios.put(
         `http://localhost:3000/journey-plans/${selectedPlan.id}`,
-        formatFormData()
+        formatFormData(),
       );
       console.log("Succefully updating a journey plan", res.data);
       getAllPlans();
@@ -99,7 +99,7 @@ export default function PlanForm({
   const handleDelete = async (e) => {
     try {
       const res = await axios.delete(
-        `http://localhost:3000/journey-plans/${selectedPlan.id}`
+        `http://localhost:3000/journey-plans/${selectedPlan.id}`,
       );
       console.log("Successfully deleted");
       getAllPlans();
@@ -117,12 +117,12 @@ export default function PlanForm({
   };
 
   return (
-    <div className="col-span-2 flex flex-col h-full bg-neutral-700 rounded-xl">
-      <div className="flex flex-row justify-between items-center h-15 px-3 shadow-xl/18 text-white">
+    <div className="col-span-2 flex h-full flex-col rounded-xl bg-neutral-700">
+      <div className="flex h-15 flex-row items-center justify-between px-3 text-white shadow-xl/18">
         <Plus
           onClick={() => setSelectedPlan(null)}
           size={40}
-          className="hover:bg-neutral-600/80 p-2 rounded transition duration-175"
+          className="rounded p-2 transition duration-175 hover:bg-neutral-600/80"
         />
         {isUpdating && (
           <X
@@ -132,12 +132,12 @@ export default function PlanForm({
               setFormData(emptyForm);
             }}
             size={40}
-            className="hover:bg-neutral-600/80 p-2 rounded transition duration-175"
+            className="rounded p-2 transition duration-175 hover:bg-neutral-600/80"
           />
         )}
       </div>
 
-      <form className="flex flex-col p-4 h-full mt-5">
+      <form className="mt-5 flex h-full flex-col p-4">
         <input
           type="text"
           name="name"
@@ -145,14 +145,14 @@ export default function PlanForm({
           autoComplete="off"
           value={formData.name}
           onChange={handleChange}
-          className="border border-neutral-600 text-gray-50 py-4 px-3 font-bold text-2xl mb-4 focus:outline-none rounded-lg"
+          className="mb-4 rounded-lg border border-neutral-600 px-3 py-4 text-2xl font-bold text-gray-50 focus:outline-none"
         />
 
-        <div className="grid grid-cols-2 gap-x-10 space-y-5 mb-2">
+        <div className="mb-2 grid grid-cols-2 space-y-5 gap-x-10">
           <div>
             <label
               htmlFor="start_date"
-              className="block text-white font-medium text-lg ml-1 mr-4 mb-1"
+              className="mr-4 mb-1 ml-1 block text-lg font-medium text-white"
             >
               Start Date
             </label>
@@ -160,7 +160,7 @@ export default function PlanForm({
               type="text"
               name="start_date"
               id="start_date"
-              className="w-full border border-neutral-600 rounded px-2 py-2 text-gray-50 focus:outline-none"
+              className="w-full rounded border border-neutral-600 px-2 py-2 text-gray-50 focus:outline-none"
               placeholder="2025-04-28"
               autoComplete="off"
               value={formData.start_date}
@@ -171,7 +171,7 @@ export default function PlanForm({
           <div>
             <label
               htmlFor="end_date"
-              className="block text-white font-medium text-lg ml-1 mr-4 mb-1"
+              className="mr-4 mb-1 ml-1 block text-lg font-medium text-white"
             >
               End Date
             </label>
@@ -179,7 +179,7 @@ export default function PlanForm({
               type="text"
               name="end_date"
               id="end_date"
-              className="w-full border border-neutral-600 rounded px-2 py-2 text-gray-50 focus:outline-none"
+              className="w-full rounded border border-neutral-600 px-2 py-2 text-gray-50 focus:outline-none"
               placeholder="2025-06-10"
               autoComplete="off"
               value={formData.end_date}
@@ -190,7 +190,7 @@ export default function PlanForm({
           <div>
             <label
               htmlFor="locations"
-              className="block text-white font-medium text-lg ml-1 mr-4 mb-1"
+              className="mr-4 mb-1 ml-1 block text-lg font-medium text-white"
             >
               Locations
             </label>
@@ -198,7 +198,7 @@ export default function PlanForm({
               type="text"
               name="locations"
               id="locations"
-              className="w-full border border-neutral-600 rounded px-2 py-2 text-gray-50 focus:outline-none"
+              className="w-full rounded border border-neutral-600 px-2 py-2 text-gray-50 focus:outline-none"
               placeholder="Dublin, Cork"
               autoComplete="off"
               value={formData.locations}
@@ -208,7 +208,7 @@ export default function PlanForm({
           <div>
             <label
               htmlFor="activities"
-              className="block text-white font-medium text-lg ml-1 mr-4 mb-1"
+              className="mr-4 mb-1 ml-1 block text-lg font-medium text-white"
             >
               Activities
             </label>
@@ -216,7 +216,7 @@ export default function PlanForm({
               type="text"
               name="activities"
               id="activities"
-              className="w-full border border-neutral-600 rounded px-2 py-2 text-gray-50 focus:outline-none"
+              className="w-full rounded border border-neutral-600 px-2 py-2 text-gray-50 focus:outline-none"
               placeholder="swimming, dancing"
               autoComplete="off"
               value={formData.activities}
@@ -228,16 +228,16 @@ export default function PlanForm({
         <textarea
           name="description"
           placeholder="description"
-          className="h-60 text-white border border-neutral-600 rounded-xl p-3 focus:outline-none"
+          className="h-60 rounded-xl border border-neutral-600 p-3 text-white focus:outline-none"
           value={formData.description}
           onChange={handleChange}
         />
-        <div className="flex flex-row  gap-x-8 mt-5 justify-between">
+        <div className="mt-5 flex flex-row justify-between gap-x-8">
           {isUpdating || (
             <button
               type="button"
               onClick={handleCreate}
-              className="w-full py-2 rounded-lg bg-violet-600 text-white font-semibold hover:bg-violet-500 transition duration-175"
+              className="w-full rounded-lg bg-violet-600 py-2 font-semibold text-white transition duration-175 hover:bg-violet-500"
             >
               Create
             </button>
@@ -246,7 +246,7 @@ export default function PlanForm({
             <button
               type="button"
               onClick={handleUpdate}
-              className="w-full py-2 rounded-lg bg-gray-100 hover:bg-gray-300 font-semibold transition duration-175"
+              className="w-full rounded-lg bg-gray-100 py-2 font-semibold transition duration-175 hover:bg-gray-300"
             >
               Update
             </button>
